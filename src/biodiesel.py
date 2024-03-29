ce69995ddcf0008f99d480180bdb3280f763f78c
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2024-03-29 09:42:29 +0800

    mhchem

--- src/biodiesel.py
+++ src/biodiesel.py
@@ -1,11 +1,27 @@
+"""
+*** biodiesel.py ***
+
+mhm yes much fuel
+"""
+
+import os
+
 from manim import *
 from manim_chemistry import *
 
+assets = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../assets")
+mols = os.path.join(assets, "mols")
+
+
 class TwoDMoleculeScene(Scene):
-   def construct(self):
-        # Example.construct(self)
+    """molecules!"""
+
+    def construct(self):
+        """construct some mols!"""
         t_grp = VGroup()
-        triglyceride = MMoleculeObject.from_mol_file("molfiles/triglyceride.mol")
+        triglyceride = MMoleculeObject.from_mol_file(
+            os.path.join(mols, "triglyceride.mol")
+        )
         t_grp.add(triglyceride)
         t_text = Text("Triglyceride", color=WHITE).next_to(triglyceride, DOWN)
         t_grp.add(t_text)
@@ -16,9 +32,9 @@ class TwoDMoleculeScene(Scene):
         t_grp.target.shift(LEFT * 4)
         self.play(MoveToTarget(t_grp), run_time=1)
         self.wait(1)
-        
+
         f_grp = VGroup()
-        fame = MMoleculeObject.from_mol_file("molfiles/FAME.mol")
+        fame = MMoleculeObject.from_mol_file(os.path.join(mols, "FAME.mol"))
         f_grp.add(fame)
         f_text = Text("Methyl Ester", color=WHITE).next_to(fame, DOWN)
         f_grp.add(f_text)
@@ -30,8 +46,10 @@ class TwoDMoleculeScene(Scene):
         arrow = Arrow([1.5, -0.5, 0], [2.5, -0.5, 0], color=WHITE)
         self.play(MoveToTarget(f_grp), GrowArrow(arrow), run_time=1)
         self.wait(1)
-        
-        methanol = MMoleculeObject.from_mol_file("molfiles/methanol.mol")
+
+        methanol = MMoleculeObject.from_mol_file(
+            os.path.join(mols, "methanol.mol")
+        )
         m_grp = VGroup()
         m_grp.add(methanol)
         m_text = Text("Methanol", color=WHITE).next_to(methanol, DOWN)
@@ -43,52 +61,51 @@ class TwoDMoleculeScene(Scene):
         m_grp.target.scale(0.5)
         self.play(MoveToTarget(m_grp), Create(plus_sign), run_time=2)
         self.wait(1)
-        
+
         everything = VGroup()
         everything.add(t_grp, f_grp, m_grp, arrow, plus_sign)
         everything.generate_target()
         everything.target.scale(0.80)
         everything.target.shift(LEFT * 1)
-        
+
         g_grp = VGroup()
-        glycerol = MMoleculeObject.from_mol_file("molfiles/glycerol.mol")
+        glycerol = MMoleculeObject.from_mol_file(
+            os.path.join(mols, "glycerol.mol")
+        )
         g_grp.add(glycerol)
         g_text = Text("Glycerol", color=WHITE).next_to(glycerol, DOWN)
         g_grp.add(g_text)
         g_grp.scale(0.4)
         g_grp.shift(RIGHT * 4.5)
-        
+
         plus_sign2 = MathTex("+").move_to([3.5, -0.5, 0]).scale(0.80)
-        
-        self.play(MoveToTarget(everything), Create(glycerol), Write(g_text), Create(plus_sign2))
+
+        self.play(
+            MoveToTarget(everything),
+            Create(glycerol),
+            Write(g_text),
+            Create(plus_sign2),
+        )
         self.wait(1)
+
 
 class Combustion(Scene):
+    """combustion equation!"""
+
     def construct(self):
-        combustion = MathTex(r"C_xH_y+(x+\frac{y}{4})O_2\to xCO_2+\frac{y}{2}H_2O")
+        preamble = TexTemplate()
+        preamble.add_to_preamble("\\usepackage[version=4]{mhchem}")
+        combustion = Tex(
+            r"\ce{C_xH_y + $\left(x + \frac{y}{4}\right)$ O2 -> $x$ CO2 + $\frac{y}{2}$ H2O}",
+            tex_template=preamble,
+        )
         self.play(Write(combustion))
         combustion.generate_target()
-        combustion.target.shift(UP*0.5)
+        combustion.target.shift(UP * 0.5)
         self.play(MoveToTarget(combustion))
         self.wait(1)
-        enthalpy_change = MathTex(r"\Delta H = \Sigma H_{products} - \Sigma H_{reactants} < 0").shift(DOWN*0.5)
+        enthalpy_change = MathTex(
+            r"\Delta H = \sum H_\text{products} - \sum H_\text{reactants} < 0"
+        ).shift(DOWN * 0.5)
         self.play(Write(enthalpy_change))
         self.wait(1)
-
-class Example(Scene):
-        def construct(self):
-            number_plane = NumberPlane( #HEREFROM
-                x_range=[-10, 10, 1],
-                y_range=[-10, 10, 1],
-                stroke_width=100,
-                axis_config = {
-                    'stroke_color': YELLOW,
-                    'stroke_width': 20,
-                },
-                background_line_style={
-                    'stroke_color': GREY,
-                    'stroke_width': 15,
-                    'stroke_opacity': 1
-                }
-            )
-            self.add(number_plane)
